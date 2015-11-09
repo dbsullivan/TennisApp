04e750d72128be005b8936fdbcc55b0817d9054e
Dave Sullivan <dbsullivan@madisoncollege.edu>
2015-11-09 15:20:39 -0600

    update 20151110

---
+++ tests/com/TennisApp/java/PlayerSearchServletTest.java
@@ -0,0 +1,78 @@
+package com.TennisApp.java;
+
+import com.TennisApp.java.persistance.PlayerDao;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Dave on 11/9/2015.
+ */
+public class PlayerSearchServletTest {
+
+    @Before
+    public void setUp() throws Exception {
+        PlayerSearchServlet pss = new PlayerSearchServlet();
+//        pss.doGet(HttpServletRequest request,
+//                HttpServletResponse response );
+    }
+
+    // negative test for parameters
+    @Test
+    public void playerSearchNullsTest() {
+        PlayerDao playerDao = new PlayerDao();
+
+        PlayerSearch playerSearch = new PlayerSearch();
+        playerSearch.setSearchTerm("");
+        playerSearch.setSearchType("");
+
+    }
+
+    // negative test for term parameter
+    @Test
+    public void playerSearchNullsTermTest() {
+        PlayerDao playerDao = new PlayerDao();
+
+        PlayerSearch playerSearch = new PlayerSearch();
+        playerSearch.setSearchTerm("");
+        playerSearch.setSearchType("id");
+
+    }
+
+    // negative test for type parameter
+    @Test
+    public void playerSearchNullsTypeTest() {
+        PlayerDao playerDao = new PlayerDao();
+
+        PlayerSearch playerSearch = new PlayerSearch();
+        playerSearch.setSearchTerm("1");
+        playerSearch.setSearchType("");
+
+    }
+
+    // positive test for ID
+    @Test
+    public void playerSearchValidIDTest() {
+        PlayerDao playerDao = new PlayerDao();
+
+        PlayerSearch playerSearch = new PlayerSearch();
+        playerSearch.setSearchTerm("1");
+        playerSearch.setSearchType("id");
+
+    }
+
+    // positive test for Name
+    @Test
+    public void playerSearchValidNameTest() {
+        PlayerDao playerDao = new PlayerDao();
+
+        PlayerSearch playerSearch = new PlayerSearch();
+        playerSearch.setSearchTerm("Player");
+        playerSearch.setSearchType("name");
+
+    }
+}
